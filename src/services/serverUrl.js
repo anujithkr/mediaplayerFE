eb3027cdbdced901c5024896197b2fe35e2459ca
anujithkr <anujithkr5@gmail.com>
2025-03-19 12:19:25 +0530

    changeurl

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,3 +1,3 @@
 //  export const serverUrl='http://localhost:5000';
 
-export const serverUrl = 'https://mediaplayerbe-1.onrender.com'
+export const serverUrl = 'https://mediaplayerbackend-i1qe.onrender.com'
